0979d27f338ce4395022d6904b560e8c12e55311
Nitesh Rai <nitesh.rai@arup.com>
2016-10-26 15:58:01 +1100

    testing

--- js/app.js
+++ js/app.js
@@ -11,7 +11,8 @@ var map = L.map('map').setView([-37.813611, 144.963055], 15);
                     g = svg.append("g").attr("class", "leaflet-zoom-hide");
 
 
-            d3.json("data/existingLine.geojson", function (geoShape) {
+            //Cityloop geojson file      
+            d3.json("data/cityloop.geojson", function (geoShape) {
                 /* Add a LatLng object to each item in the dataset */
 
                 var transform = d3.geo.transform({point: projectPoint}),
@@ -30,7 +31,6 @@ var map = L.map('map').setView([-37.813611, 144.963055], 15);
                 function reset() {
 
                     bounds = path.bounds(geoShape);
-                    console.log(bounds[0]);
 
                     var topLeft = bounds[0],
                             bottomRight = bounds[1];
@@ -58,8 +58,57 @@ var map = L.map('map').setView([-37.813611, 144.963055], 15);
 
             })
 
+            //melMetro geojson file
+            d3.json("data/melMetroLine.geojson", function (geoMelMetroShape) {
+                /* Add a LatLng object to each item in the dataset */
+
+                var transform = d3.geo.transform({point: projectPoint}),
+                        path = d3.geo.path().projection(transform);
+
+                // create path elements for each of the features
+                d3_features = g.selectAll("path")
+                        .data(geoMelMetroShape.features)
+                        .enter().append("path");
+
+                map.on("viewreset", reset);
+
+                reset();
+
+                // fit the SVG element to leaflet's map layer
+                function reset() {
+
+                    bounds = path.bounds(geoMelMetroShape);
+
+                    var topLeft = bounds[0],
+                            bottomRight = bounds[1];
+
+                    svg.attr("width", bottomRight[0] - topLeft[0])
+                            .attr("height", bottomRight[1] - topLeft[1])
+                            .style("left", topLeft[0] + "px")
+                            .style("top", topLeft[1] + "px");
+
+                    g.attr("transform", "translate(" + -topLeft[0] + ","
+                            + -topLeft[1] + ")");
+
+                    // initialize the path data 
+                    d3_features.attr("d", path)
+                            .style("stroke", "yellow")
+                            .style("stroke-width", 3)
+                            .style("fill-opacity", 0);
+                }
+
+                // Use Leaflet to implement a D3 geometric transformation.
+                function projectPoint(x, y) {
+                    var point = map.latLngToLayerPoint(new L.LatLng(y, x));
+                    this.stream.point(point.x, point.y);
+                }
+
+            })
+
+
+            //Existing stations geojson
             d3.json("data/station.geojson", function (collection) {
-                
+
                 /* Add a LatLng object to each item in the dataset */
                 collection.features.forEach(function (d) {
                     d.LatLng = new L.LatLng(d.geometry.coordinates[1],
@@ -82,13 +131,14 @@ var map = L.map('map').setView([-37.813611, 144.963055], 15);
                         .style("stroke-width", 2)
                         .on('mouseover', tip.show)
                         .on('mouseout', tip.hide)
-                        .style("fill", "blue")
-                        .attr("r", 12);
+                        .style("fill", "blue");
+
 
                 map.on("viewreset", update);
                 update();
 
                 function update() {
+
                     feature.attr("transform",
                             function (d) {
                                 return "translate(" +
@@ -96,5 +146,14 @@ var map = L.map('map').setView([-37.813611, 144.963055], 15);
                                         map.latLngToLayerPoint(d.LatLng).y + ")";
                             }
                     )
+                    .attr("r", function(d){
+                        if (d.properties.STATION === 'Melbourne Central' ||
+                            d.properties.STATION === 'Flinders Street') {
+                            return 30;
+                        } else {
+                            return 10;
+                        }
+
+                    })
                 }
             })
